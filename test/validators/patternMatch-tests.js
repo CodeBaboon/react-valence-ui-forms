d1d922abd12cf38044612b07d2a206f6a13020c0
Dave Batiste <dave.batiste@d2l.com>
2015-04-09 15:39:34 -0400

    Adding tests for patternMatch validator.

---
+++ test/validators/patternMatch-tests.js
@@ -0,0 +1,43 @@
+'use strict';
+
+jest.dontMock('../../lib/validators/patternMatch');
+
+var React = require('react/addons'),
+	TestUtils = React.addons.TestUtils,
+	Validation = require('../../lib/index');
+
+describe('patternMatch validator', function() {
+
+	it('is defined', function() {
+
+		expect(Validation.Validators.patternMatch).toBeDefined();
+
+	});
+
+	it('returns isValid=true when initialized invalid value is undefined', function() {
+
+		var validator = Validation.Validators.patternMatch();
+
+		expect(validator(null, 'some invalid').isValid).toBeTruthy();
+
+	});
+
+	it('returns isValid=true when a valid value is provided', function() {
+
+		var validator = Validation.Validators.patternMatch(/apples/, 'a message');
+
+		expect(validator(null, 'apples').isValid).toBeTruthy();
+
+	});
+
+	it('returns isValid=false when an invalid value is provided', function() {
+
+		var validator = Validation.Validators.patternMatch(/apples/, 'a message');
+
+		var result = validator(null, 'bananas');
+		expect(result.isValid).toBeFalsy();
+		expect(result.message).toBe('a message');
+
+	});
+
+});
